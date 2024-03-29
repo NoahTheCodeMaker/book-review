c3e6e0aa52c75c73293074c2170fbc50f0d4d3f5
NoahDragoon <ndragoon@acelynk.com>
2024-03-29 07:07:33 -0400

    Add Star Rating Component

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,7 +25,7 @@ class BookController extends Controller
             'popular_last_6_months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6_months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
         $books = $books->get();
 
@@ -51,13 +51,17 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cacheKey = 'book:' . $book->id;
+        $cacheKey = 'book:' . $id;
 
-        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+        $book = cache()->remember(
+            $cacheKey, 3600, 
+            fn() => 
+            Book::with([
             'reviews' => fn ($query) => $query->latest()
-        ]));
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
         
         return view('books.show', ['book' => $book]);
     }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,11 +20,23 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder
     {
         return $query->withCount([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ])
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withReviewsCount()
             ->orderBy('reviews_count', 'desc');
     }
 
@@ -35,9 +47,7 @@ class Book extends Model
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg([
-            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ], 'rating')
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
@@ -78,5 +88,11 @@ class Book extends Model
         return $query->highestRated(now()->subMonths(6), now())
             ->popular(now()->subMonths(6), now())
             ->minReviews(5);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn (Book $book) => cache()->forget('book:' . $book->id));
+        static::deleted(fn (Book $book) => cache()->forget('book:' . $book->id));
     }
 }
